c7d2bf049f8ae956645ff03e90185b2a8c973b43
nekit270 <126622890+nekit270ch@users.noreply.github.com>
2023-04-29 00:38:30 +0300

    Add files via upload

--- patcherUtils.js
+++ patcherUtils.js
@@ -7,43 +7,90 @@ if(!fs.existsSync('www/texturepacks')){
 try{
 	window.pconf = JSON.parse(fs.readFileSync('patcher.json'));
 }catch(_){
-	fs.writeFileSync('patcher.json', '{\r\n    texture_pack: "none"\r\n}');
-	window.pconf = { texture_pack: 'none' };
+	window.pconf = {
+		texture_pack: 'none',
+		keys: {
+			PAUSE: 'p',
+			SAVE: 's',
+			RESTART: 'r',
+			JS_CONSOLE: 'j',
+			GOD_MODE: 'g',
+			ADD_MONEY: 'm',
+			NO_CLIP: 'n',
+			SET_SPEED: 'e',
+			HELP: 'h'
+		}
+	}
+	fs.writeFileSync('patcher.json', JSON.stringify(pconf, null, 4));
 }
 
 window.loadedTextures = [];
 window.godMode = false;
 
-window.onkeypress = e=>{
-	if(e.key == 'j'){
-		let inp = prompt('Введите команду');
-		if(!inp || inp.length == 0) return;
-		try{
-			alert(eval(inp));
-		}catch(e){
-			alert(e);
+window.addEventListener('keypress', e=>{
+	switch(e.key){
+		case pconf.keys.JS_CONSOLE: {
+			let inp = prompt('Введите команду');
+			if(!inp || inp.length == 0) return;
+			try{
+				alert(eval(inp));
+			}catch(e){
+				alert(e);
+			}
+			break;
 		}
-	}else if(e.key == 's'){
-		SceneManager.push(Scene_Save);
-	}else if(e.key == 'g'){
-		window.godMode = true;
-		alert('Режим бога включен');
-	}else if(e.key == 'm'){
-		$gameParty._gold = Number.MAX_SAFE_INTEGER;
-		alert('Деньги накручены');
-	}else if(e.key == 'p'){
-		alert('ПАУЗА\n\nНажмите ОК, чтобы возобновить');
-	}else if(e.key == 'h'){
-		alert('Горячие клавиши RPGM Patcher\n\n[P] Поставить игру на паузу\n[R] Перезапустить игру\n[T] Открыть окно выбора текстурпака\n[J] Открыть консоль JavaScript\n[S] Открыть окно сохранения\n[G] Режим бога (очень высокие HP и атака)\n[M] Накрутка денег');
-	}else if(e.key == 'b'){
-		alert(loadedTextures.join('\n'));
-	}else if(e.key == 'r'){
-		location.reload();
-	}else if(e.key == 't'){
-		let p = prompt('Введите название текстурпака', pconf.texture_pack);
-		if(!p) return;
-		pconf.texture_pack = p;
-		fs.writeFileSync('patcher.json', JSON.stringify(pconf, null, 4));
-		location.reload();
+
+		case pconf.keys.SAVE: {
+			SceneManager.push(Scene_Save);
+			break;
+		}
+
+		case pconf.keys.GOD_MODE: {
+			window.godMode = true;
+			alert('Режим бога включен');
+			break;
+		}
+
+		case pconf.keys.ADD_MONEY: {
+			$gameParty._gold = Number.MAX_SAFE_INTEGER;
+			alert('Деньги накручены');
+			break;
+		}
+
+		case pconf.keys.PAUSE: {
+			alert('ПАУЗА\n\nНажмите ОК, чтобы возобновить');
+			break;
+		}
+
+		case pconf.keys.RESTART: {
+			location.reload();
+			break;
+		}
+
+		case pconf.keys.NO_CLIP: {
+			$gamePlayer.canPass = ()=>true;
+			break;
+		}
+
+		case pconf.keys.SET_SPEED: {
+			let inp = prompt('Введите новую скорость', $gamePlayer._moveSpeed);
+			if(inp && !isNaN(+inp)){
+				$gamePlayer._moveSpeed = +inp;
+			}
+			break;
+		}
+
+		case pconf.keys.HELP: {
+			let str = 'Горячие клавиши RPGM Patcher\n\n';
+			str += `[${pconf.keys.PAUSE.toUpperCase()}] Поставить игру на паузу\n`;
+			str += `[${pconf.keys.RESTART.toUpperCase()}] Перезапустить игру\n`;
+			str += `[${pconf.keys.JS_CONSOLE.toUpperCase()}] Открыть консоль JavaScript\n`;
+			str += `[${pconf.keys.SAVE.toUpperCase()}] Открыть окно сохранения\n`;
+			str += `[${pconf.keys.GOD_MODE.toUpperCase()}] Режим бога (очень высокие HP, атака, защита и т.д.)\n`;
+			str += `[${pconf.keys.ADD_MONEY.toUpperCase()}] Накрутка денег\n`;
+			str += `[${pconf.keys.NO_CLIP.toUpperCase()}] NoClip (возможность проходить сквозь объекты)\n`;
+			str += `[${pconf.keys.SET_SPEED.toUpperCase()}] Изменить скорость игрока\n`;
+			alert(str);
+		}
 	}
-};
+});
